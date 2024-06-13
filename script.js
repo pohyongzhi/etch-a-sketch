1370511cfe620e97a8cfbb8d1aa0908c07cddf02
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-06-13 22:35:35 +0800

    Added an opacity decrease after certain interactions

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
 // Query container element
 const container = document.querySelector(".container");
 
-let grids = 100;
+let grids = 16;
+let opacity = 1;
+let count = 0;
 
 // Create 16 rows
-for (let row = 1; row < grids; row++) {
-    // Create 16 column
-    const tempRowDiv = document.createElement("div");
-    tempRowDiv.className = "row";
+function drawGrids(grids) {
+    for (let row = 1; row < grids; row++) {
+        // Create 16 column
+        const tempRowDiv = document.createElement("div");
+        tempRowDiv.className = "row";
 
-    for (let col = 1; col < grids; col++) {
-        const tempColDiv = document.createElement("div");
-        tempColDiv.className = "col";
-        tempColDiv.id = "r-" + row + "c-" + col;
+        for (let col = 1; col < grids; col++) {
+            const tempColDiv = document.createElement("div");
+            tempColDiv.className = "col";
+            tempColDiv.id = "r-" + row + "c-" + col;
 
-        tempRowDiv.append(tempColDiv);
+            tempRowDiv.append(tempColDiv);
+        }
+
+        container.append(tempRowDiv);
     }
-
-    container.append(tempRowDiv);
 }
-
-// Mouse hover event
-document.addEventListener("mouseover", (event) => {
-    const colorCode = generateColorCode();
-    const targetId = "#" + event.target.id;
-    console.log(colorCode);
-
-    let div = document.querySelector(targetId);
-    div.style.backgroundColor = colorCode;
-});
 
 function generateColorCode() {
     const alphabets = [
@@ -60,3 +54,39 @@ function generateColorCode() {
 
     return colorCode;
 }
+
+// Add button event
+const button = document.querySelector("button");
+button.addEventListener("click", (event) => {
+    // Prompt user for no. of grids
+    const userInput = prompt("Please enter a grid size between 1 - 100");
+
+    if (userInput >= 1 && userInput <= 100) {
+        // Amend the grid size to user input
+        grids = userInput;
+
+        // Remove elements in the container from the dom
+        container.innerHTML = "";
+
+        // Draw a new grid
+        drawGrids(grids);
+    }
+});
+
+// Mouse hover event
+document.addEventListener("mouseover", (event) => {
+    const colorCode = generateColorCode();
+    const targetId = "#" + event.target.id;
+
+    let div = document.querySelector(targetId);
+    div.style.backgroundColor = colorCode;
+
+    if (opacity != 0) {
+        opacity -= 0.005;
+    }
+
+    div.style.opacity = opacity;
+});
+
+// Main function
+drawGrids(grids);
